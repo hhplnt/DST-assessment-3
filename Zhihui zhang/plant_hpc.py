5730fe2df2e6ebd85fcda76d2e3991ada699a6cf
ZhihuiZhang1 <haileyzhang1@outlook.com>
2023-04-12 14:27:59 +0100

    Create plant_hpc.py

---
+++ Zhihui zhang/plant_hpc.py
@@ -0,0 +1,256 @@
+# %%
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+
+
+
+
+# %% [markdown]
+# # load the data
+
+# %%
+X_train_1=np.load(file="C:/Users/haile/OneDrive - University of Bristol/assessment3/DST-assessment-3.1/DST-assessment-3/Zhihui zhang/X_train_1.npy")/255
+X_test_1=np.load(file="C:/Users/haile/OneDrive - University of Bristol/assessment3/DST-assessment-3.1/DST-assessment-3/Zhihui zhang/X_test_1.npy")/255
+Y_train_1=np.load(file="C:/Users/haile/OneDrive - University of Bristol/assessment3/DST-assessment-3.1/DST-assessment-3/Zhihui zhang/Y_train_1.npy")
+Y_test_1=np.load(file="C:/Users/haile/OneDrive - University of Bristol/assessment3/DST-assessment-3.1/DST-assessment-3/Zhihui zhang/Y_test_1.npy")
+
+# %% [markdown]
+# # inception
+# 
+
+# %%
+import tensorflow.keras.backend as K
+
+
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+# %%
+EPOCHS = 100
+INIT_LR = 1e-3
+BS = 32
+default_image_size = tuple((224,224))
+image_size = 0
+#directory_root = '/kaggle/input/vegnet-organized-dataset-of-cauliflower-disease'
+width=224
+height=224
+depth=3
+n_classes=15
+
+# %%
+aug = ImageDataGenerator(
+    rotation_range=25, width_shift_range=0.1,
+    height_shift_range=0.1, shear_range=0.2, 
+    zoom_range=0.2,horizontal_flip=True, 
+    fill_mode="nearest")
+
+# %%
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import Dense
+from keras.applications.inception_v3 import InceptionV3
+from keras.utils.np_utils import to_categorical
+# model = tf.keras.backend.clear_session()
+
+
+# Get the InceptionV3 model so we can do transfer learning
+base_inception = InceptionV3(weights='imagenet', include_top=False, input_shape= (224,224,3))
+                             
+# Add a global spatial average pooling layer
+out = base_inception.output
+out = GlobalAveragePooling2D()(out)
+out = Dense(512, activation='relu')(out)
+out = Dense(512, activation='relu')(out)
+predictions = Dense(n_classes, activation='softmax')(out)
+model = Model(inputs=base_inception.input, outputs=predictions)
+
+# only if we want to freeze layers
+for layer in base_inception.layers:
+    layer.trainable = False
+    
+# Compile 
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,metrics=['accuracy', f1_m,  precision_m, recall_m]) 
+
+
+
+# %%
+# train the network
+print("[INFO] training INCEPTION network...")
+history = model.fit(
+    aug.flow(X_train_1, Y_train_1, batch_size=BS),
+    validation_data=(X_test_1, Y_test_1),
+    steps_per_epoch=len(X_train_1) // BS,
+    epochs=1
+    )
+
+# %%
+import plotly.graph_objects as go
+def display_training_curves(training, validation, yaxis):
+    ylabel = "{}".format(yaxis)
+    title = "{} vs. Epochs".format(yaxis)
+        
+    fig = go.Figure()
+        
+    fig.add_trace(
+        go.Scatter(x=np.arange(1, EPOCHS+1), mode='lines+markers', y=training, marker=dict(color="dodgerblue"),
+               name="Train"))
+    
+    fig.add_trace(
+        go.Scatter(x=np.arange(1, EPOCHS+1), mode='lines+markers', y=validation, marker=dict(color="darkorange"),
+               name="Val"))
+    
+    fig.update_layout(title_text=title, yaxis_title=ylabel, xaxis_title="Epochs", template="plotly_white")
+    fig.show()
+
+# %%
+
+display_training_curves(
+    history.history['accuracy'], 
+    history.history['val_accuracy'], 
+    'accuracy')
+display_training_curves(
+    history.history['loss'], 
+    history.history['val_loss'], 
+    'loss')
+display_training_curves(
+    history.history['precision_m'], 
+    history.history['val_precision_m'], 
+    'precision')
+display_training_curves(
+    history.history['recall_m'], 
+    history.history['val_recall_m'], 
+    'recall')
+display_training_curves(
+    history.history['f1_m'], 
+    history.history['val_f1_m'], 
+    'f1 score')
+
+
+
+# %% [markdown]
+# # VGG16 Model
+
+# %%
+import tensorflow as tf
+from keras.models import Model
+from keras.applications import vgg16
+from keras.optimizers import Adam
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import Dense
+from keras.applications.inception_v3 import InceptionV3
+from keras.utils.np_utils import to_categorical
+model = tf.keras.backend.clear_session()
+# Get the Resnet model so we can do transfer learning
+base_model = vgg16.VGG16(weights= None, include_top=False, input_shape= (224,224,3))
+# model= None                          
+# Add a global spatial average pooling layer
+out = base_model.output
+out = GlobalAveragePooling2D()(out)
+out = Dense(512, activation='relu')(out)
+out = Dense(512, activation='relu')(out)
+predictions = Dense(n_classes, activation='softmax')(out)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# only if we want to freeze layers
+for layer in base_inception.layers:
+    layer.trainable = False
+    
+# Compile 
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,metrics=['accuracy', f1_m,  precision_m, recall_m]) 
+model.summary()
+
+# %% [markdown]
+# # train the network
+
+# %%
+print("[INFO] training VGG16 network...")
+history = model.fit(
+    aug.flow(X_train_1, Y_train_1, batch_size=BS),
+    validation_data=(X_test_1, Y_test_1),
+    steps_per_epoch=len(X_train_1) // BS,
+    epochs=1
+    )
+
+# %% [markdown]
+# # display curves
+
+# %%
+def display_training_curves(training, validation, yaxis):
+    ylabel = "{}".format(yaxis)
+    title = "{} vs. Epochs".format(yaxis)
+        
+    fig = go.Figure()
+        
+    fig.add_trace(
+        go.Scatter(x=np.arange(1, EPOCHS+1), mode='lines+markers', y=training, marker=dict(color="dodgerblue"),
+               name="Train"))
+    
+    fig.add_trace(
+        go.Scatter(x=np.arange(1, EPOCHS+1), mode='lines+markers', y=validation, marker=dict(color="darkorange"),
+               name="Val"))
+    
+    fig.update_layout(title_text=title, yaxis_title=ylabel, xaxis_title="Epochs", template="plotly_white")
+    fig.show()
+
+# %%
+
+display_training_curves(
+    history.history['accuracy'], 
+    history.history['val_accuracy'], 
+    'accuracy')
+display_training_curves(
+    history.history['loss'], 
+    history.history['val_loss'], 
+    'loss')
+display_training_curves(
+    history.history['precision_m'], 
+    history.history['val_precision_m'], 
+    'precision')
+display_training_curves(
+    history.history['recall_m'], 
+    history.history['val_recall_m'], 
+    'recall')
+display_training_curves(
+    history.history['f1_m'], 
+    history.history['val_f1_m'], 
+    'f1 score')
+
+# %%
+# evaluate the model
+loss, accuracy, f1_score, precision, recall = model.evaluate(X_test_1, Y_test_1, verbose=0)
+print("Accuracy: {} \nLoss:{} \nF1_score: {} \nPrecision: {} \nRecall: {}".format(accuracy, loss, f1_score, precision, recall))
+
+# %% [markdown]
+# # save model
+# 
+
+# %%
+def Res_Plot_Test_Save(name):
+    print("[INFO] Calculating model accuracy")
+    scores = model.evaluate(X_test_1, Y_test_1)
+    print(f"Test Accuracy: {scores[1]*100}")
+    print("[INFO] Saving model")
+    model.save(name)
+
+# %%
+Res_Plot_Test_Save('VGG16_(Imagenet_Weights).h5')
+
+
